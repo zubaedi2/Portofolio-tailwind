43ba982fcb226f6b60b765767e4035eab3fd0fcd
zubaedi2 <127464504+zubaedi2@users.noreply.github.com>
2023-07-14 19:18:32 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#6ee7b7",
+        dark: "#475569",
+        secondary: "#64748b",
+      },
+      screen: {
+        "2xl": "1320px",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
